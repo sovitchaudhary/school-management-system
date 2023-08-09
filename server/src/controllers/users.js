1a8758abd69f0e0ff493f2b773583e33a0eca40c
sovitchaudhary <sovitchaudhary45@gmail.com>
2023-08-10 02:54:20 +0530

    Added hashPassword feature

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,16 +1,20 @@
-const Users =require('../models/users')
-const registerNewUser = async(req, res) => {
+const Users = require("../models/users");
+const bcrypt = require('bcrypt');
+const registerNewUser = async (req, res) => {
   // email already exits
   const matched = await Users.exists({ email: req.body.email });
   if (matched) {
     res.status(409).json({
-      msg: 'Email already exits',
+      msg: "Email already exits",
     });
   } else {
+    //encrypt the password
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashPassword
     await Users.create(req.body);
     res.status(201).json({
-      msg: 'User Created Successfully',
+      msg: "User Created Successfully",
     });
   }
-  };
-  module.exports = {registerNewUser}
+};
+module.exports = { registerNewUser };
