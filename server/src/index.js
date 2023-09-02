9b5540bd4316b51b68213d2a1642b60a62f5ccc3
sovitchaudhary <sovitchaudhary45@gmail.com>
2023-09-02 22:29:10 +0530

    Resolving CORS Error

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const userRoutes = require('./routes/users')
+const cors = require("cors");
 const connectDb = require('./db/connection')
 const app = express();
 require('dotenv').config()
+
+app.use(cors());
 app.use(express.json());
 
 app.use(userRoutes)
+
 connectDb();
 const port = process.env.PORT
 app.listen(port, () => {
